e7f673008a8f063f3cd84d3f2b215282c0d3728e
GBOKO-git <gboko.amara@uvci.edu.ci>
2025-05-05 01:40:07 +0000

    fix: update app listen

--- frontend/src/Components/productCard.jsx
+++ frontend/src/Components/productCard.jsx
@@ -79,7 +79,7 @@ export const ProductCard = ({ produit }) => {
               <Link href={`/product/${_id}`} >
               <button
                 
-                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium
+                className="text-white bg-purple-700/100 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium
                  rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800
                  hover:cursor-pointer"
               >
--- frontend/src/app/ShoppingCartItems/page.jsx
+++ frontend/src/app/ShoppingCartItems/page.jsx
@@ -1,11 +1,14 @@
+import Produit from "@/Components/accueil/Produit";
+
 const { default: ShoppingCart } = require("@/Components/ShoppingCart")
 
 
 const ShoppingCartRoute = () => {
     return(
         <>
-        <div className="min-h-screen  bg-[url('/logo/ecme.png')] bg-cover bg-center">
+        <div className="min-h-screen  bg-[url('/logo/ecme.png')] bg-cover bg-center pt-20 pb-5">
         <ShoppingCart/>
+        <Produit/>
         </div>
         </>
     )
